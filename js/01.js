529a0f7b4d685bfd943336e751e77145aa11eeab
Freelancer <se.salgado@duocuc.cl>
2023-02-02 20:03:53 -0300

    variables en JavaScript

---
+++ js/01.js
@@ -0,0 +1,18 @@
+// Variables
+
+var producto = 'Audifonos Gamer';// Iniciar Variable y asignarle valor
+
+var disponible; //Iniciamos variables sin valor
+
+producto = true //Reasignamos valor
+
+var producto1 = 'Computador',//Podemos crear variables con distintos valores
+    disponible1 = true,
+    categoria = 'Computadoras';
+
+//Reglas de las variables
+//No pueden iniciar con numeros(1disponible-NO)
+//Estilo para Variables
+var nombre_producto = 'Monitor HD';//este se conoce como underscore
+var nombreProducto = 'Monitor HD';//este se conoce como Camelcase
+var nombreproducto = 'Monitor HD';//este se conoce comno Pascal Case
