9835724ad868be35136a03a755afe7b2d974138e
Freelancer <se.salgado@duocuc.cl>
2023-02-02 20:30:14 -0300

    cadena de texto o String

---
+++ js/04.js
@@ -0,0 +1,6 @@
+//String o cadenas de texto
+const producto = 'Monitor de 20"'//esta es la mas usada
+//Otra forma de hacer lo mismo 
+const producto2 = String('Monitor de 30 Pulgadas')
+//Una tercera forma de hacer lo mismo
+const producto3 = new String('Monitor de 40 pulgadas')
