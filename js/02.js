62917a6f4d2b55148759e00fa85c6a9892b79400
Freelancer <se.salgado@duocuc.cl>
2023-02-02 20:09:29 -0300

    variables let

---
+++ js/02.js
@@ -0,0 +1,17 @@
+//Variables con let no tiene diferencia con var
+let producto = 'Audifonos Gamer';// Iniciar Variable let y asignarle valor
+
+let disponible; //Iniciamos variables let sin valor
+
+producto = true //Reasignamos valor
+
+let producto1 = 'Computador',//Podemos crear variables con let distintos valores
+    disponible1 = true,
+    categoria = 'Computadoras';
+
+//Reglas de las variables let
+//No pueden iniciar con numeros(1disponible-NO)
+//Estilo para Variables let
+let nombre_producto = 'Monitor HD';//este se conoce como underscore
+let nombreProducto = 'Monitor HD';//este se conoce como Camelcase
+let nombreproducto = 'Monitor HD';//este se conoce comno Pascal Case
