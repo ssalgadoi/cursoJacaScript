f6970ee09aa7edce35ef559d7dac47fba1aa56b3
Freelancer <se.salgado@duocuc.cl>
2023-02-02 20:19:54 -0300

    variable const

---
+++ js/03.js
@@ -0,0 +1,17 @@
+//Variables const 
+const producto = 'Audifonos Gamer';// Iniciar Variable const y asignarle valor
+
+const disponible = true; //No se puede inicializar un const sin valor(const disponible; asi NO)
+
+producto = true //No se Reasignamos valor
+
+const producto1 = 'Computador',//Podemos crear variables con const distintos valores
+    disponible1 = true,
+    categoria = 'Computadoras';
+
+//Reglas de las variables let
+//No pueden iniciar con numeros(1disponible-NO)
+//Estilo para Variables let
+const nombre_producto = 'Monitor HD';//este se conoce como underscore
+const nombreProducto = 'Monitor HD';//este se conoce como Camelcase
+const nombreproducto = 'Monitor HD';//este se conoce comno Pascal Case
