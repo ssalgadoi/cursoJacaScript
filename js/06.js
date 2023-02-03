089cbcde5399df8bae0b0335676836a4345468a4
Freelancer <se.salgado@duocuc.cl>
2023-02-03 17:43:52 -0300

    trabajamos funciones matematicas

---
+++ js/06.js
@@ -0,0 +1,18 @@
+//Objeto Math
+
+let resultado;
+
+resultado = Math.PI;
+resultado = Math.round(2.5);//Redondea con la tabla normal 5+ o 5-
+resultado = Math.ceil(2.1);//Redondeamos hacia arriba
+resultado = Math.floor(2.9);//Redonde siempre hacia abajo
+resultado = Math.sqrt(144);//Para encontra la raiz cuadrada
+resultado = Math.abs(-200);//Para convertir numeros negativo en positivo
+resultado = Math.min(3, 5, 1, 9, 2, 7);//Este nos dice cual es el menor numero
+resultado = Math.max(3, 5, 1, 9, 2, 7);//Este nos da el numero mas grande del listado
+resultado = Math.random();//Este seleccionba un numero al azar
+resultado = Math.floor( Math.random() * 30);//Podemos mesclar estas funciones
+
+
+
+console.log(resultado);
