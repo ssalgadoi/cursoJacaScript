b483d6c509a07236c96271bd5ffe32f0fd49bc69
Freelancer <se.salgado@duocuc.cl>
2023-02-03 18:16:08 -0300

    Incremento y decrementos puntaje

---
+++ js/07.js
@@ -0,0 +1,20 @@
+//Orden de las operaciones
+
+let resultado;
+
+resultado = 20 + 30 * 2;//este multiplica 30*2 y luego suma 20
+resultado = ( 20 + 30 ) * 2;//este suma 20+30 y el sesultado multiplica *2
+resultado = ( 100 + 200 + 300 ) * .2;//si quicieramos realizar descuento del 20% en una compra
+resultado = ( 100 + 200 + 300 ) * 1.19;//Si quicieramos sumarle el IVA al producto
+
+
+// console.log(resultado);
+
+let puntaje = 10;
+
+puntaje++;//Puntaje + incremento en 1
+++puntaje;//Imcremento + puntaje
+puntaje--;//Se quita puntaje
+--puntaje;//Lo que se quita 
+puntaje += 10;//Este nos el 10 + puntaje 10
+console.log(puntaje);
