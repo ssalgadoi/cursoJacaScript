c2e8b5e9856b01dd9e27bdd90cb11cca78645a4f
Freelancer <se.salgado@duocuc.cl>
2023-02-05 16:18:30 -0300

    Objetos

---
+++ js/10.js
@@ -0,0 +1,23 @@
+// Objetos
+
+// se pueden crear variables por separado
+// estas son tres variable
+const nombreProducto = "Monitor 20 pulgada"
+const precio = 300;
+const disponible = true;
+
+
+// se pueden crear obtejos juntos
+// estas tres variables juntas hacen un objeto
+const producto = {
+    nombreProducto : "Monitor 20 Pulgadas",
+    precio : 300,
+    disponible : true
+}
+
+console.log(producto);
+
+// sintaxis de puntos
+console.log(producto.precio);
+console.log(producto.disponible);
+console.log(producto.nombreProducto);
